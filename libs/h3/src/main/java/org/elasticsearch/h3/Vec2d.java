ef0295f2046d51a553c7dd4da61a652891abb024
iverase <ivera@apache.org>
2023-01-11 17:11:51 +0100

    Protect H3 library from integer overflow

--- libs/h3/src/main/java/org/elasticsearch/h3/Vec2d.java
+++ libs/h3/src/main/java/org/elasticsearch/h3/Vec2d.java
@@ -196,17 +196,17 @@ final class Vec2d {
                     j = m2;
                 } else {
                     i = m1;
-                    j = m2 + 1;
+                    j = Math.incrementExact(m2);
                 }
             } else {
                 if (r2 < (1.0 - r1)) {
                     j = m2;
                 } else {
-                    j = m2 + 1;
+                    j = Math.incrementExact(m2);
                 }
 
                 if ((1.0 - r1) <= r2 && r2 < (2.0 * r1)) {
-                    i = m1 + 1;
+                    i = Math.incrementExact(m1);
                 } else {
                     i = m1;
                 }
@@ -216,21 +216,21 @@ final class Vec2d {
                 if (r2 < (1.0 - r1)) {
                     j = m2;
                 } else {
-                    j = m2 + 1;
+                    j = Math.addExact(m2, 1);
                 }
 
                 if ((2.0 * r1 - 1.0) < r2 && r2 < (1.0 - r1)) {
                     i = m1;
                 } else {
-                    i = m1 + 1;
+                    i = Math.incrementExact(m1);
                 }
             } else {
                 if (r2 < (r1 / 2.0)) {
-                    i = m1 + 1;
+                    i = Math.incrementExact(m1);
                     j = m2;
                 } else {
-                    i = m1 + 1;
-                    j = m2 + 1;
+                    i = Math.incrementExact(m1);
+                    j = Math.incrementExact(m2);
                 }
             }
         }
@@ -241,18 +241,18 @@ final class Vec2d {
             if ((j % 2) == 0)  // even
             {
                 int axisi = j / 2;
-                int diff = i - axisi;
-                i = i - 2 * diff;
+                int diff = Math.subtractExact(i, axisi);
+                i = Math.subtractExact(i, Math.multiplyExact(2, diff));
             } else {
-                int axisi = (j + 1) / 2;
-                int diff = i - axisi;
-                i = i - (2 * diff + 1);
+                int axisi = Math.addExact(j, 1) / 2;
+                int diff = Math.subtractExact(i, axisi);
+                i = Math.subtractExact(i, Math.addExact(Math.multiplyExact(2, diff), 1));
             }
         }
 
         if (y < 0.0) {
-            i = i - (2 * j + 1) / 2;
-            j = -1 * j;
+            i = Math.subtractExact(i, Math.addExact(Math.multiplyExact(2, j), 1) / 2);
+            j = Math.multiplyExact(-1, j);
         }
         final CoordIJK coordIJK = new CoordIJK(i, j, k);
         coordIJK.ijkNormalize();
